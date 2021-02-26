c356f19305abdae95ebbad988eb22a0c39e5093c
Jonas Schlauch <mrjosch99@gmail.com>
2021-02-26 13:04:05 +0100

    added routes file

---
+++ lib/routes.dart
@@ -0,0 +1,19 @@
+class Routes {
+
+  static const loading = 'loading';
+  static const signIn = 'signIn';
+  static const signUp = 'signUp';
+  static const home = 'home';
+  static const profile = 'profile';
+  static const invite = 'invite';
+  static const gameHistory = 'gameHistory';
+  static const friends = 'friends';
+  static const settings = 'settings';
+  static const aboutUs = 'aboutUs';
+  static const socialMedia = 'socialMedia';
+  static const createGame = 'createGame';
+  static const inGame = 'inGame';
+  static const checkoutDetails = 'checkoutDetails';
+  static const stats = 'stats';
+
+}
