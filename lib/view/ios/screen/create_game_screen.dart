6833c35a98c7efe21334f6e0375fb3247bc578b6
Jonas Schlauch <mrjosch99@gmail.com>
2021-02-26 16:03:40 +0100

    added basic navigation

--- lib/view/ios/screen/create_game_screen.dart
+++ lib/view/ios/screen/create_game_screen.dart
@@ -1,10 +1,33 @@
+import 'package:dart_counter/app_language.dart';
+import 'package:dart_counter/app_state.dart';
+import 'package:dart_counter/routes.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
 
 class CreateGameScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      child: Center(child: Text(this.toStringShort() + " -- IOS")),
-    );
+    return Consumer<AppState>(builder: (context, state, child) {
+      return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          leading: CupertinoButton(
+            padding: EdgeInsets.zero,
+            child: Icon(CupertinoIcons.chevron_back, size: 35),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          middle: Text(state.settings.language.createGame),
+        ),
+        child: SafeArea(
+            child: Column(children: [
+          CupertinoButton(
+              child: Text('InGame'),
+              onPressed: () {
+                Navigator.pushNamed(context, Routes.inGame);
+              }),
+        ])),
+      );
+    });
   }
 }
--- lib/view/ios/screen/home_screen.dart
+++ lib/view/ios/screen/home_screen.dart
@@ -1,20 +1,63 @@
 import 'package:dart_counter/app_language.dart';
 import 'package:dart_counter/app_state.dart';
+import 'package:dart_counter/res/my_flutter_app_icons.dart';
+import 'package:dart_counter/routes.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer<AppState>(
-      builder: (context, state, child) {
-        return CupertinoPageScaffold(
-          navigationBar: CupertinoNavigationBar(
-            middle: Text(state.settings.language.settings),
+    return Consumer<AppState>(builder: (context, state, child) {
+      return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          leading: CupertinoButton(
+            padding: EdgeInsets.zero,
+            child: Icon(MyFlutterApp.menu, size: 35),
+            onPressed: () {},
           ),
-          child: Center(child: Text(this.toStringShort() + " -- IOS")),
-        );
-      }
-    );
+          middle: Text(state.settings.language.home),
+        ),
+        child: SafeArea(
+            child: Column(children: [
+          CupertinoButton(
+              child: Text('Profile'),
+              onPressed: () {
+                Navigator.pushNamed(context, Routes.profile);
+              }),
+          CupertinoButton(
+              child: Text('CreateGame'),
+              onPressed: () {
+                Navigator.pushNamed(context, Routes.createGame);
+              }),
+          CupertinoButton(
+              child: Text('SocialMedia'),
+              onPressed: () {
+                Navigator.pushNamed(context, Routes.socialMedia);
+              }),
+          CupertinoButton(
+              child: Text('Settings'),
+              onPressed: () {
+                Navigator.pushNamed(context, Routes.settings);
+              }),
+          CupertinoButton(
+              child: Text('Invite'),
+              onPressed: () {
+                Navigator.pushNamed(context, Routes.invite);
+              }),
+          CupertinoButton(
+              child: Text('Friends'),
+              onPressed: () {
+                Navigator.pushNamed(context, Routes.friends);
+              }),
+          CupertinoButton(
+              child: Text('AboutUs'),
+              onPressed: () {
+                Navigator.pushNamed(context, Routes.aboutUs);
+              }),
+        ])),
+      );
+    });
   }
 }
