0abccd3d170b6c7f7ca1bbc89ce956785ca556e6
Jonas Schlauch <mrjosch99@gmail.com>
2021-02-26 12:35:05 +0100

    added export of screens as a library for each platform

---
+++ lib/view/android/screens.dart
@@ -0,0 +1,17 @@
+library android_screens;
+
+export 'screen/about_us_screen.dart';
+export 'screen/checkout_details_screen.dart';
+export 'screen/create_game_screen.dart';
+export 'screen/friends_screen.dart';
+export 'screen/game_history_screen.dart';
+export 'screen/home_screen.dart';
+export 'screen/in_game_screen.dart';
+export 'screen/invites_screen.dart';
+export 'screen/loading_screen.dart';
+export 'screen/profile_screen.dart';
+export 'screen/settings_screen.dart';
+export 'screen/sign_in_screen.dart';
+export 'screen/sign_up_screen.dart';
+export 'screen/social_media_screen.dart';
+export 'screen/stats_screen.dart';
