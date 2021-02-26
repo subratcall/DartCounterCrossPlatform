a522420efb79a688fe15cc55b3215dda28542590
Jonas Schlauch <mrjosch99@gmail.com>
2021-02-26 18:10:40 +0100

    Added AppColors

--- lib/res/app_colors.dart
+++ lib/res/app_colors.dart
@@ -1,1 +1,15 @@
+import 'dart:ui' show Color;
 
+class AppColors {
+
+  static const Color black = Color.fromRGBO(0, 0, 0, 1.0);
+  static const Color black1 = Color.fromRGBO(22, 20, 20, 1.0);
+  static const Color black2 = Color.fromRGBO(52, 52, 50, 1.0);
+  static const Color blue = Color.fromRGBO(0, 122, 255, 1.0);
+  static const Color gray = Color.fromRGBO(228, 228, 228, 1.0);
+  static const Color green = Color.fromRGBO(50, 164, 79, 1.0);
+  static const Color red = Color.fromRGBO(204 , 40, 28, 1.0);
+  static const Color white = Color.fromRGBO(255, 255, 255, 1.0);
+  static const Color yellow = Color.fromRGBO(245, 189, 68, 1.0);
+  
+}
