a810a0d5ac8c90e522b07dc90b33c84f7a269f40
Jonas Schlauch <mrjosch99@gmail.com>
2021-03-01 14:42:42 +0100

    implemented ResetPasswordScreen and ResetPasswordSuccess screens

--- lib/view/ios/screen/authentication/reset_password_screen.dart
+++ lib/view/ios/screen/authentication/reset_password_screen.dart
@@ -1,3 +1,5 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:dart_counter/assets/app_colors.dart';
 import 'package:dart_counter/assets/app_images.dart';
 import 'package:dart_counter/view/ios/widget/primary_button.dart';
 import 'package:dart_counter/view/ios/widget/textfield.dart';
@@ -17,6 +19,7 @@ class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
   @override
   Widget build(BuildContext context) {
     return Screen<ResetPasswordViewModel>(builder: (context, model, child) {
+      //return ResetPasswordSuccess();
       return CupertinoPageScaffold(
         child: SafeArea(
           child: Padding(
@@ -40,11 +43,26 @@ class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
                 Spacer(
                   flex: 67,
                 ),
-                Expanded(child: Text("H1"), flex: 21),
+                Expanded(
+                  child: AutoSizeText(
+                    AppLocalizations.of(context).forgotPassword,
+                    maxLines: 1,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 23),
+                  ),
+                  flex: 21,
+                ),
                 Spacer(
                   flex: 28,
                 ),
-                Expanded(child: Text("H2"), flex: 32),
+                Expanded(
+                  child: AutoSizeText(
+                    AppLocalizations.of(context).insertEmailToResetPassword,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 14),
+                    maxLines: 2,
+                  ),
+                  flex: 32,
+                ),
                 Spacer(
                   flex: 28,
                 ),
@@ -144,54 +162,73 @@ class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
 }
 
 
-// On Succes show this view
-/**
-
+class ResetPasswordSuccess extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-    child: SafeArea(
-    child: Padding(
-    padding: const EdgeInsets.symmetric(horizontal: 25.0),
-    child: Column(
-    mainAxisSize: MainAxisSize.max,
-    children: [
-    Spacer(
-    flex: 105,
-    ),
-    Expanded(
-    child: Row(
-    children: [
-    Spacer(flex: 120,),
-    Flexible(child: Image.asset(AppImages.logo), flex: 135,),
-    Spacer(flex: 120,),
-    ],
-    ),
-    flex: 166,
-    ),
-    Spacer(
-    flex: 88,
-    ),
-    Expanded(child: Text("H1"), flex: 21),
-    Spacer(
-    flex: 28,
-    ),
-    Expanded(child: Text("H2"), flex: 32),
-    Spacer(
-    flex: 93,
-    ),
-    Expanded(
-    child: PrimaryButton(
-    text: AppLocalizations.of(context).login,
-    onPressed: () => model.onConfirmPressed(email: emailController.text),
-    ),
-    flex: 50,
-    ),
-    Spacer(
-    flex: 158,
-    ),
-    ],
-    ),
-    ),
-    ),
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Spacer(
+                flex: 105,
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    Spacer(flex: 120,),
+                    Flexible(child: Image.asset(AppImages.logo), flex: 135,),
+                    Spacer(flex: 120,),
+                  ],
+                ),
+                flex: 166,
+              ),
+              Spacer(
+                flex: 64,
+              ),
+              Expanded(
+                child: Icon(
+                    CupertinoIcons.checkmark_alt_circle,
+                  color: AppColors.green,
+                  size: 60,
+                ),
+                flex: 44,
+              ),
+              Spacer(
+                flex: 22,
+              ),
+              Expanded(
+                  child: AutoSizeText(
+                      AppLocalizations.of(context).emailSent,
+                      maxLines: 1,
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 23),
+                  ),
+                  flex: 21,
+              ),
+              Spacer(
+                flex: 28,
+              ),
+              Expanded(
+                child: AutoSizeText(
+                    AppLocalizations.of(context).checkMailAccountForPasswordResetLink,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 14),
+                    maxLines: 2,
+                ),
+                flex: 32,
+              ),
+              Spacer(
+                flex: 71,
+              ),
+              Spacer(
+                flex: 188,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
-
-**/
+  }
+}
