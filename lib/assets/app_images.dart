e0f4c3db22724bab970d1db36079c943ddc1035f
Jonas Schlauch <mrjosch99@gmail.com>
2021-03-02 13:04:48 +0100

    add assets

--- lib/assets/app_images.dart
+++ lib/assets/app_images.dart
@@ -6,4 +6,10 @@ class AppImages {
   static const facebook = 'assets/images/png/facebook.png';
   static const instagram = 'assets/images/png/instagram.png';
   static const google = 'assets/images/png/google.png';
+  static const target = 'assets/images/png/target.png';
+  static const win = 'assets/images/png/win.png';
+  static const defeat = 'assets/images/png/defeat.png';
+  static const trendUp = 'assets/images/png/trend_up.png';
+  static const trendDown = 'assets/images/png/trend_down.png';
+  static const delete = 'assets/images/png/delete.png';
 }
