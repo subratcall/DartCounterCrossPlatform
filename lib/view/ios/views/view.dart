a614bb47b09c76a00375b8ff1f92807f89d28a14
Jonas Schlauch <mrjosch99@gmail.com>
2021-03-04 12:02:49 +0100

    made content view size available using mediaquery in cupertinoview

--- lib/view/ios/views/view.dart
+++ lib/view/ios/views/view.dart
@@ -4,21 +4,36 @@ import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class CupertinoView extends StatelessWidget {
   final CupertinoNavigationBar navigationBar;
-  final EdgeInsets padding;
   final Widget child;
   final VoidCallback onTap;
 
-  CupertinoView({this.navigationBar, this.padding = const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0), this.child, this.onTap});
+  CupertinoView({this.navigationBar, this.child, this.onTap});
 
   @override
   Widget build(BuildContext context) {
+    // Params to calc contentview width and height
+    final double width = MediaQuery.of(context).size.width;
+    final double height = MediaQuery.of(context).size.height;
+    final double paddingTop = MediaQuery.of(context).padding.top;
+    final double navbarHeight = navigationBar != null ? navigationBar.preferredSize.height : 0;
+    final double paddingBottom = MediaQuery.of(context).padding.bottom;
+
     return CupertinoScaffold(
       body: CupertinoPageScaffold(
         navigationBar: navigationBar,
         child: GestureDetector(
           onTap: onTap,
           child: SafeArea(
-            child: Padding(padding: padding, child: child),
+            child: MediaQuery(
+              data: MediaQueryData(
+                size: Size(width, height - paddingTop - navbarHeight - paddingBottom),
+              ),
+              child: SizedBox(
+                width: width,
+                  height: height - paddingTop - navbarHeight - paddingBottom,
+                  child: child,
+              ),
+            ),
           ),
         ),
       ),
