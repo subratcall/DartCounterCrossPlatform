42bc8c81ea257a8c144fb1ac8437d6de26a01ce1
Jonas Schlauch <mrjosch99@gmail.com>
2021-03-13 20:41:08 +0100

    add test dummies to package dart_game

---
+++ packages/dart_game/test/unit/model/game_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('methodName', () {
+    test('Given TODO When TODO Then TODO', () {
+      // Arrange
+
+      // Act
+
+      // Assert
+
+    });
+  });
+}
