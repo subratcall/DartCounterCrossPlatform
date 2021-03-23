7498eeaad87c2e142dd4bb6084b1a7e41650f6ee
Jonas Schlauch <mrjosch99@gmail.com>
2021-03-23 06:36:39 +0100

    implemented + tested model package todo undo throw 6 testst still fail todo finishes.dart

--- packages/dart_game/test/unit/model/throw_test.dart
+++ packages/dart_game/test/unit/model/throw_test.dart
@@ -1,14 +1,21 @@
+import 'package:dart_game/test/lib.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('methodName', () {
-    test('Given TODO GIVEN TODO WHEN isOneDartFinish is called TODO', () {
-      // Arrange
-
-      // Act
+  group('constructor', () {
+    test('GIVEN valid points WHEN throw is created THEN dartsThrown = 3', () {
+      // Arrange & Act
+      final Throw t = Throw(100);
 
       // Assert
+      expect(t.dartsThrown, 3);
+    });
+    test('GIVEN valid points WHEN throw is created THEN dartsThrown = 0', () {
+      // Arrange & Act
+      final Throw t = Throw(100);
 
+      // Assert
+      expect(t.dartsOnDouble, 0);
     });
   });
 }
